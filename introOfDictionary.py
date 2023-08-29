2cefd3fae961f9a9e012ffa997f1fc926939dc11
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-29 20:35:54 +0600

    Explain dictionary

--- introOfDictionary.py
+++ introOfDictionary.py
@@ -1,3 +1,11 @@
 '''
-What is dictionary in python:
+What is dictionary in python: Dictionary is a ordered key:value collective data type.
+In a dictionary:
+key must be a immutable data type like string.
+value can be hold any type of data.
+duplicate key is not allowed. Py hold last duplicate value in pairs
+we can create a dictionary by useing dict function and curly braces
+student=dict(list[tuple,tuple])
+student=dict([('name','MSA'),('age',24)])
+student={"name":"MSA","age":24}
 '''
