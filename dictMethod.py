263da82c5ed79b1431017c24ac81bde8f45a6ec5
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-29 22:03:44 +0600

    del dict and dict item

--- dictMethod.py
+++ dictMethod.py
@@ -13,3 +13,26 @@ infoDict=dict(infoList)
 
 # versity=infoDict.get('versity','not found')
 # print(versity) # not found : infoDict e versity exist kore na
+
+#removing dictionary item
+'''
+kuno dictionary er specific item remove/delete korar jonno del, del item, clear, pop, 
+popitem use kora zay.
+puro dict computer memmory theke elete korar jonno "del" key word use kora zete pare. 
+del infoDict.
+dict theke kuno specified item delete korar jonno del dectName[keyName] use kora hoy
+kuno dict ke empty korar jonno dictName.clear() method use kora zay. clear() method 
+emty dict return kore.
+kuno dict theke specified specified item remove korar jonn dictName.pop('keyName') use kora zay.
+dictName.pop(keyName) method pop kora key er value return kore.
+kuno dict theke randomly kuno key:value remove korar jonno dictName.pop() method use 
+kora zay.
+'''
+# del dict and dict specified item
+# del infoDict['salary'] infoDict theke salary delete hobe.
+# print(infoDict) #{'name': 'MSA', 'age': 24, 'institute': 'NSTU'}
+
+# del infoDict
+# print(infoDict) # showing name error.
+
+
