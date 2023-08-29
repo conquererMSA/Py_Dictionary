fcf177d905fbd38c09e603c9db5fb8e2fcb42ff4
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-29 20:49:55 +0600

    count length of a dictionary

--- introOfDictionary.py
+++ introOfDictionary.py
@@ -1,6 +1,8 @@
 '''
-What is dictionary in python: Dictionary is a ordered key:value collective data type.
+What is dictionary in python: Dictionary is a unordered key:value collective data type.
 In a dictionary:
+dict are iterable.
+keys are case-sensative.
 key must be a immutable data type like string.
 value can be hold any type of data.
 duplicate key is not allowed. Py hold last duplicate value in pairs
@@ -13,3 +15,11 @@ student=dict([("name","MSA"),("age",24),("institute","NSTU")])
 # print(student) # {'name': 'MSA', 'age': 24, 'institute': 'NSTU'}
 # type_of_student=type(student)
 # print(type_of_student) #<class 'dict'>
+
+#count length of a dictionary
+# dict_length=0
+# for key in student:
+#     dict_length+=1
+# print(dict_length) # 3 item in student dict
+# print(len(student))
+
