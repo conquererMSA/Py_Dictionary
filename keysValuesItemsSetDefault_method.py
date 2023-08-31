2520c98e934adbb91231e597c64dc272611c98eb
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-31 22:41:36 +0600

    dict.setdefault(key,defaultValue)

--- keysValuesItemsSetDefault_method.py
+++ keysValuesItemsSetDefault_method.py
@@ -2,6 +2,11 @@
 In that file I explored some dict methods:
 dict.keys(): kuno ekti dictionary er sob gulu keys ke ektiti list er moddye return kore.
 ei list ti <class 'dict_keys'> theke ase
+
+dict.setdefault(key,defaultValue): setdefault method e kuno dict er kuno ekti key dile
+tar value return kore, zodi sei key dict e exist na kore tobe tobe default value diey
+sei name e dict e ekti key:value pairs insert kore. Zodi setdefault method e
+defaultValue na thake thobe key er value None hoy.
 '''
 #keys method
 students={'MSA':223,'PVS':342,'MHD':432}
@@ -25,9 +30,16 @@ for value in studentValues:
 
 #items method: key:value pairs ke tuple e vore return kore.
 listOfTupleOfKeyValuesPair=students.items()
-print(listOfTupleOfKeyValuesPair) #dict_items([('MSA', 223), ('PVS', 342), ('MHD', 432)])
+# print(listOfTupleOfKeyValuesPair) #dict_items([('MSA', 223), ('PVS', 342), ('MHD', 432)])
 listOfTupleOfKeyValuesPairs=list(students.items())
 # print(listOfTupleOfKeyValuesPairs) #[('MSA', 223), ('PVS', 342), ('MHD', 432)]
 for keyValueTuple in listOfTupleOfKeyValuesPairs:
     infoString=F"{keyValueTuple[0]} has {keyValueTuple[1]} fees"
-    print(infoString)
+    # print(infoString)
+
+#setdefault method:
+newStudents=students.setdefault('ALD',456)
+# print(newStudents)
+# print(students)
+students.setdefault('SMD',234)
+# print(students) #{'MSA': 223, 'PVS': 342, 'MHD': 432, 'ALD': 456, 'SMD': 234}
